b5804eaffb744e424288fd43ea9ec9918be1cf2f
Leonardo <martins.leonardo3415@gmail.com>
2026-08-06 11:05:14 -0300

    Estrutura inicial do projeto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main className="container py-5">
+      <h1 className="text-primary">React + TypeScript</h1>
+
+      <button className="btn btn-primary">
+        Continuar
+      </button>
+    </main>
+  )
+}
