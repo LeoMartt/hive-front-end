d4a9e4b67186ecc5c15bccb7f9dcaf4de01384ba
Guilherme Defavori Fabretti <guidfabretti@hotmail.com>
2026-08-11 20:39:40 -0300

    feat: add ActivityStatusBadge component

---
+++ src/components/activities/ActivityStatusBadge.tsx
@@ -0,0 +1,14 @@
+import { ACTIVITY_STATUS_BADGE_CLASS, ACTIVITY_STATUS_LABELS } from "../../utils/activityIndicators";
+import type { ActivityStatus } from "../../types/activity";
+
+interface ActivityStatusBadgeProps {
+  status: ActivityStatus;
+}
+
+export default function ActivityStatusBadge({ status }: ActivityStatusBadgeProps) {
+  return (
+    <span className={`activity-badge ${ACTIVITY_STATUS_BADGE_CLASS[status]}`}>
+      {ACTIVITY_STATUS_LABELS[status]}
+    </span>
+  );
+}
