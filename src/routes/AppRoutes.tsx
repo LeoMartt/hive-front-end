53d0c25c6a3847903a5799ca0916ddf9472f81a4
Guilherme Defavori Fabretti <guidfabretti@hotmail.com>
2026-08-12 20:18:21 -0300

    feat: wire ProjectLayout with nested routes for the specific-project area

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,24 @@
 import { Navigate, Route, Routes } from "react-router";
 import ProjectsPage from "../pages/ProjectsPage";
-import ProjectDetailPage from "../pages/ProjectDetailPage";
+import ProjectLayout from "../layouts/ProjectLayout";
+import ProjectActivitiesPage from "../pages/ProjectActivitiesPage";
+import ActivityDetailPlaceholderPage from "../pages/ActivityDetailPlaceholderPage";
+import PlaceholderPage from "../pages/PlaceholderPage";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/projetos" replace />} />
       <Route path="/projetos" element={<ProjectsPage />} />
-      <Route path="/projetos/:id" element={<ProjectDetailPage />} />
+      <Route path="/projetos/:id" element={<ProjectLayout />}>
+        <Route index element={<Navigate to="atividades" replace />} />
+        <Route path="dashboard" element={<PlaceholderPage title="O dashboard do projeto" />} />
+        <Route path="atividades" element={<ProjectActivitiesPage />} />
+        <Route path="atividades/:activityId" element={<ActivityDetailPlaceholderPage />} />
+        <Route path="estrutura" element={<PlaceholderPage title="A estrutura (WBS) do projeto" />} />
+        <Route path="issues" element={<PlaceholderPage title="As issues do projeto" />} />
+        <Route path="config" element={<PlaceholderPage title="A configuração do projeto" />} />
+      </Route>
     </Routes>
   );
 }
